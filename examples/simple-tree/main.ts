835f53f084c48e9622b134a348b3ace857bc7485
André "MG" Wisén <andre.wisen@gmail.com>
2024-08-12 20:12:32 +0200

    Refactor LineNetwork example to include new nodes and links in "simple-tree" visualization

--- examples/simple-tree/main.ts
+++ examples/simple-tree/main.ts
@@ -14,8 +14,12 @@ const nodes: Node[] = [
   new Node(2, new THREE.Vector3(10, 0, 0)),
   // B - C
   new Node(3, new THREE.Vector3(20, 0, 5)),
+  // C - E
+  new Node(4, new THREE.Vector3(30, 0, 5)),
   // B - D
-  new Node(4, new THREE.Vector3(20, 0, -5)),
+  new Node(5, new THREE.Vector3(20, 0, -5)),
+  // D - F
+  new Node(6, new THREE.Vector3(30, 0, -5)),
 ];
 
 const links: Link[] = [
@@ -24,7 +28,13 @@ const links: Link[] = [
   // B - C
   new Link(2, 2, 3),
   // B - D
-  new Link(3, 2, 4),
+  new Link(3, 2, 5),
+  // C - E
+  new Link(4, 5, 6),
+  // D - F
+  new Link(5, 3, 4),
+  // Connect
+  new Link(6, 4, 6),
 ];
 
 network.init(nodes, links, example.group);
