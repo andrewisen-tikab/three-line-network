90c30568578b6f6d6393130d0727da37fb77b497
André "MG" Wisén <andre.wisen@gmail.com>
2024-08-14 17:34:29 +0200

    Refactor vite.config.ts to improve build process and add dts plugin

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  build: {
+    sourcemap: true,
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "ThreeMultiSelect",
+      // the proper extensions will be added
+      fileName: "three-multi-select",
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["three", "three-mesh-bvh"],
+    },
+  },
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+});
